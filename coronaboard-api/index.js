f72529c050d4ada5a88b9ea07709ce0d3cf46106
ChaeYi <chaeyi990731@gmail.com>
2024-01-17 16:27:31 +0900

    서버 구축

--- coronaboard-api/index.js
+++ coronaboard-api/index.js
@@ -1,2 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
+const app = express();
+
+//Content-Type이 application/json 인 HTTP 요청의 바디를 파싱 할 수 있도록 설정
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello CoronaBoard!' });
+});
+
+const port = process.env.PORT || 8080; // 기본 값을 8080으로 지정
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}.`);
+});
